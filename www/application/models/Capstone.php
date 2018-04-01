fae41a0b2970419158cd593f740c3dd8a22757f2
todd <tjb5185@rit.edu>
2018-04-01 12:07:22 -0400

    getCapstoneAll pulls all capstone data besides the status history

--- www/application/models/Capstone.php
+++ www/application/models/Capstone.php
@@ -14,4 +14,14 @@ class Capstone extends CI_Model {
     function testing() {
         return json_encode( DB::query("SELECT * FROM roles"));
     }
+
+    function getCapstoneAll() {
+        return json_encode(DB::query("SELECT u.first_name , u.last_name, cs.title, cs.description, cs.plagerism_score, cs.grade, cs.type
+                                      FROM capstone cs JOIN student s ON cs.student_id = s.id
+                                      JOIN user u ON s.uid = u.uid;"));
+    }
+
+    function getCapstoneStudent($student){
+
+    }
 }
