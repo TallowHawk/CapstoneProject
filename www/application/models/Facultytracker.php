bb39e790548db234c890a9573089f6a42df46f87
todd <todd.bednarczyk@gmail.com>
2018-04-02 00:01:53 -0400

    updated comments and swapped signature variables because the order was confusing

--- www/application/models/Facultytracker.php
+++ www/application/models/Facultytracker.php
@@ -11,7 +11,7 @@ class Facultytracker extends CI_Model {
      * @param $fac_id - the faculty id that corresponds with the capstone id
      * @return mixed - rows affected
      */
-    function addToTracker($cap_id,$fac_id) {
+    function addToTracker($fac_id,$cap_id) {
         return DB::insert('faculty_tracker', array(
             "cap_id" => $cap_id,
             "fac_id" => $fac_id
@@ -22,9 +22,9 @@ class Facultytracker extends CI_Model {
      * Removes the selected faculty member from the selected capstone's watchlist
      * @param $cap_id - the capstone id to add to the tracker
      * @param $fac_id - the faculty id that corresponds with the capstone id
-     * @return mixed - rows affected
+     * @return mixed - boolean if the row(s) were deleted or not
      */
-    function removeFromTracker($cap_id,$fac_id){
+    function removeFromTracker($fac_id,$cap_id){
         return DB::delete('faculty_tracker', "cap_id=%i AND fac_id=%i",$cap_id,$fac_id);
     }
 
