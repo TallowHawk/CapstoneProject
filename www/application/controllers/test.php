1c9f5e6eaadda20f316503ed04ade20ef8a255e0
Josh Schrader <schraderjosh6999@gmail.com>
2018-04-02 16:06:46 -0400

    completed getName and getUsername methods

--- www/application/controllers/test.php
+++ www/application/controllers/test.php
@@ -4,6 +4,6 @@ class test extends CI_Controller {
 
 	public function index() {
 		$this->load->model('User');
-        echo json_encode($this->User->getGeneralData(6));
+        echo json_encode($this->User->getUsername(3));
 	}
 }
--- www/application/models/User.php
+++ www/application/models/User.php
@@ -1,14 +1,15 @@
 <?php
 
 class User extends CI_Model {
+
     function __construct() {
         parent::__construct();
     }
 
+
     function testing() {
         return json_encode( DB::query("SELECT * FROM Capstone"));
     }
-
 
 
     function createAccount($username, $password, $phone, $inactiveDate, $role, $fName, $lName){
@@ -23,9 +24,22 @@ class User extends CI_Model {
         ));
     }
 
+
     function getGeneralData($uid){
         return DB::query("SELECT u.username, u.phone, u.inactive_date, r.role_description, u.first_name, u.last_name FROM user u
         JOIN roles r ON r.id = u.role_id
         WHERE u.uid = %i", $uid);
+    }
+
+
+    function getName($uid){
+        return DB::query("SELECT CONCAT(first_name, ' ', last_name) FROM user
+        WHERE uid = %i", $uid);
+    }
+
+
+    function getUsername($uid){
+        return DB::query("SELECT username FROM user
+        WHERE uid = %i", $uid);
     }
 }
