95e165bad7ff1008f95cdb63c96bf4619106d10a
todd <todd.bednarczyk@gmail.com>
2018-04-01 23:47:33 -0400

    added showTrackedProjects method and started comments for addToTracker

--- www/application/models/Facultytracker.php
+++ www/application/models/Facultytracker.php
@@ -5,6 +5,12 @@ class Capstone extends CI_Model {
         parent::__construct();
     }
 
+    /**
+     * Adds the faculty member to the watchlist for a particular capstone
+     * @param $cap_id - the capstone id to add to the tracker
+     * @param $fac_id - the faculty id that corresponds with the capstone id
+     * @return mixed - rows affected
+     */
     function addToTracker($cap_id,$fac_id) {
         return DB::insert('faculty_tracker', array(
             "cap_id" => $cap_id,
@@ -14,5 +20,9 @@ class Capstone extends CI_Model {
 
     function removeFromTracker($cap_id,$fac_id){
         return DB::delete('faculty_tracker', "cap_id=%i AND fac_id=%i",$cap_id,$fac_id);
+    }
+
+    function showTrackedProjects($fac_ID) {
+        return DB::query("SELECT cap_id FROM faculty_tracker WHERE fac_id = %i",$fac_ID);
     }
 }
