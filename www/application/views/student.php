8057626ea3304c191d454789a46da71d8c2f098c
Josh Schrader <schraderjosh6999@gmail.com>
2018-04-14 18:22:03 -0400

    project details and status fields in place in student view

--- www/application/views/student.php
+++ www/application/views/student.php
@@ -1,1 +1,42 @@
 
+<div class="row">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-4 no-padding">
+        <div class="col-sm-12">
+            <div class="project-details-wrapper">
+                <div class="col-sm-12">
+                    <div class="project-details-header">
+                        <h2>Project Details</h2>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="project-details-body">
+                        <p>Project body Placeholder</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4 no-padding">
+        <div class="col-sm-12">
+            <div class="project-status-wrapper">
+                <div class="col-sm-12">
+                    <div class="project-status-header">
+                        <h2>Project Status</h2>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="project-status-body">
+                        <p>Project Status Placeholder</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-2"></div>
+    <div class="col-sm-12">
+        <div class="committee-management-wrapper">
+
+        </div>
+    </div>
+</div>
