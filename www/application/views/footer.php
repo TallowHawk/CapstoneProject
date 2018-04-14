0180949e9aebb5080b2d178fc0a6967fac77592d
Josh Schrader <schraderjosh6999@gmail.com>
2018-04-14 17:44:19 -0400

    rough footer implemented

--- www/application/views/footer.php
+++ www/application/views/footer.php
@@ -1,6 +1,10 @@
 <?php
 
 ?>
-        
+        <footer class="footer row">
+            <div class="col-sm-12">
+                <div class='footer-label'>&copy DROP DATABASE<div>
+            </div>
+        </footer>
     </body>
 </html>
