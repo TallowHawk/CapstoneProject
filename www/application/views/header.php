96c5fce843239506ca57319040b05cbc90218271
Josh Schrader <schraderjosh6999@gmail.com>
2018-04-14 16:14:39 -0400

    Header test

---
+++ www/application/views/header.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Header Page</title>
+    </head>
+    <body>
+        <p>this is a test for the header</p>
+    </body>
+</html>
