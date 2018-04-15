1f31b1dc49f0a0e4056a2ca247afadb33c200950
ryandwren <32821971+ryandwren@users.noreply.github.com>
2018-04-15 14:08:47 -0400

    basic form created

--- www/application/views/create_capstone.php
+++ www/application/views/create_capstone.php
@@ -1,2 +1,64 @@
 <?php ?>
-<h1>I work</h1>
+<h2>Hello, {Student}</h2>
+
+<form id ="create_capstone"
+		method="post"
+		onsubmit=""
+	>
+
+	<span>First Name:</span>
+	<input
+		name="fName"
+		id="fName"
+		maxlength="20"
+		size="20"
+		placeholder=""
+		value=""
+	/>
+	
+	<span>Last Name:</span>
+	<input
+		name="lName"
+		id="lName"
+		maxlength="20"   
+		size="20"
+		placeholder=""
+		value=""
+	/>
+	
+	<span>Title:</span>
+	<input
+		name="title"
+		id="title"
+		maxlength="20"   
+		size="20"
+		placeholder=""
+		value=""
+	/>
+	
+	<span>Desired Defence Date:</span>
+	<input
+		name="defenceDate"
+		id="defenceDate"
+		maxlength="10"   
+		size="10"
+		placeholder="MM/DD/YYYY"
+		value=""
+	/>
+	
+	<span>Description:</span>
+	<textarea
+		name="description"
+		id="description"
+		rows="40"   
+		cols="20"
+		style="vertical-align: top;"
+	/>
+	</textarea>
+	
+	
+	<input
+		type="submit"
+	/>
+	
+</form>
