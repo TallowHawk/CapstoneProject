fc6e9bb6c584299a7fecd0c2afc4039f34c0633a
todd <todd.bednarczyk@gmail.com>
2018-04-14 21:21:30 -0400

    Changed around createAccount in the User Model and added functionality to the Admin.createAccount controller.

--- www/application/views/create_account.php
+++ www/application/views/create_account.php
@@ -5,40 +5,40 @@
     <form>
         <div class="form-group">
             <label for="firstName">First Name</label>
-            <input type="text" class="form-control" id="firstName">
+            <input type="text" class="form-control" id="firstName" name="fname" required>
         </div>
         <div class="form-group">
             <label for="lastName">Last Name</label>
-            <input type="text" class="form-control" id="lastName">
+            <input type="text" class="form-control" id="lastName" name="lname" required>
         </div>
         <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" class="form-control" id="username">
+            <input type="text" class="form-control" id="username" name="username" required>
         </div>
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" id="password">
+            <input type="password" class="form-control" id="password" name="password" required>
         </div>
         <div class="form-group">
             <label for="phone">Phone number</label>
-            <input type="text" class="form-control" id="phone">
+            <input type="text" class="form-control" id="phone" name="phone" required>
         </div>
 
         <div class="radio">
             <label>
-                <input type="radio" name="userType" id="student">
+                <input type="radio" name="userType" id="student" value="student" checked>
                 Student
             </label>
         </div>
         <div class="radio">
             <label>
-                <input type="radio" name="userType" id="faculty">
+                <input type="radio" name="userType" id="faculty" value="faculty">
                 Faculty
             </label>
         </div>
         <div class="radio disabled">
             <label>
-                <input type="radio" name="userType" id="staff">
+                <input type="radio" name="userType" id="staff" value="staff">
                 Staff
             </label>
         </div>
