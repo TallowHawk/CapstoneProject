fe0bd5e878a5464af8f2de242a042f4500b83aa8
todd <todd.bednarczyk@gmail.com>
2018-04-14 20:09:02 -0400

    Started working on Admin functionality to create an account.

---
+++ www/application/views/create_account.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+<main class="col-sm-12">
+    <form>
+        <div class="form-group row">
+            <label for="fName" class="col-sm-2 col-form-label">First Name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="fName" name="fName">
+            </div>
+            <label for="lName" class="col-sm-2 col-form-label">Last Name</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="lName" name="lName">
+            </div>
+            <label for="username" class="col-sm-2 col-form-label">username</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="username" name="username">
+            </div>
+            <label for="phone" class="col-sm-2 col-form-label">phone</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="phone" name="phone">
+            </div>
+        </div>
+    </form>
+</main>
