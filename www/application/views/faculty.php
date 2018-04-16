316ddd3d000a1dd71614015e4325045e9a74d6f5
todd <todd.bednarczyk@gmail.com>
2018-04-15 22:18:21 -0400

    Added tracking list to the faculty.

--- www/application/views/faculty.php
+++ www/application/views/faculty.php
@@ -119,6 +119,42 @@
             </div>
             <div class="col-sm-2"></div>
         </div>
+        <div class="col-sm-12 section-spacer">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+                <div class="faculty-tracking-list-wrapper section-border clearfix">
+                    <div class="col-sm-12 no-padding">
+                            <div class="col-sm-8">
+                                <h2>Tracking List</h2>
+                            </div>
+                            <div class="col-sm-4">
+                                <div class="tracking-add-btn-wrapper">
+                                    <button type="button" name="faculty-add-committee-btn">Add</button>
+                                </div>
+                            </div>
+                    </div>
+
+                    <div class="tracking-list-field clearfix">
+                        <div class="col-sm-4">
+                            <div class="tracking-list-name">
+                                <h4>{Student Name}</h4>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="tracking-list-name">
+                                <h4>{Capstone Title}</h4>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="tracking-remove-btn-wrapper">
+                                <button type="button" name="faculty-remove-tracking-btn">Remove</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2"></div>
+        </div>
     </div>
     <div class="logout-btn-wrapper">
         <div class="logout-btn" onclick="window.location.assign('<?php echo base_url(); ?>login/logout/');">
