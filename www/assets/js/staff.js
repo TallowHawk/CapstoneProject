3ade08941f3e1b0a142c64e53dd8ebeb3faa3695
todd <todd.bednarczyk@gmail.com>
2018-04-16 23:07:28 -0400

    Added capstone details

--- www/assets/js/staff.js
+++ www/assets/js/staff.js
@@ -8,7 +8,10 @@ let staff = {
         }).done(function (json) {
             console.log(json);
             let projectDetails = document.getElementsByClassName("project-details-wrapper")[0];
-            document.getElementById()
+            document.getElementById("staff-proj-det-name").innerText = json.first_name + " " + json.last_name;
+            document.getElementById("staff-proj-det-title").innerText = json.title;
+            document.getElementById("staff-proj-det-description").innerText = json.description;
+            document.getElementById("staff-proj-det-defense").innerText = json.defense_date;
         });
     }
 };
