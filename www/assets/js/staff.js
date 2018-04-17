4e5ff95390fc6e9907a7ce4019375f6bb09d7f65
todd <tjb5185@rit.edu>
2018-04-17 12:58:25 -0400

    Now loading defense dates. And it's loading the capstone when the view button is pressed

--- www/assets/js/staff.js
+++ www/assets/js/staff.js
@@ -59,6 +59,42 @@ let staff = {
                 $('#myModal').modal('hide');
             });
         });
+    },
+
+    defenseDateModal: function () {
+        let modalBody = "";
+        modalBody += "<div class='col-sm-12 modal-labels'><div class='col-sm-4'><h3>Defense Date</h3></div>";
+        modalBody += "<div class='col-sm-4'><h3>Title</h3></div></div>";
+
+        $(".modal-body").html(modalBody);
+        $('#myModal').modal('show');
+
+        $.ajax({
+            url: ajaxURLStart + "api/getCapstoneDefenseDates/",
+            method: "get",
+            dataType: "json"
+        }).done(function (json) {
+            let ajaxAddition = "";
+            console.log(json);
+
+            $.each(json, function (i, ele) {
+                ajaxAddition += "<div class='col-sm-12'><div class='modal-cap-project clearfix'>";
+                ajaxAddition += "<div class='col-sm-4'><div class='modal-cap-title'>";
+                ajaxAddition += "<h4>" + ele.defense_date + "</h4></div></div>";
+                ajaxAddition += "<div class='col-sm-4'><div class='modal-cap-username'>";
+                ajaxAddition += "<h4>" + ele.title + "</h4></div></div>";
+                ajaxAddition += "<div class='col-sm-4'><div class='modal-cap-view-div'>";
+                ajaxAddition += "<button type='button' data-user='" + ele.username + "' name='modal-cap-view-btn'>VIEW</button>";
+                ajaxAddition += "</div></div></div></div>";
+            });
+            $(".modal-body").append(ajaxAddition);
+
+            $(".modal-cap-view-div button").attr('name', 'modal-cap-view-btn').on('click', function () {
+                let username = $(this).attr("data-user");
+                staff.getCapstone(username);
+                $('#myModal').modal('hide');
+            });
+        });
     }
 };
 $(document).ready(function() {
@@ -71,5 +107,9 @@ $(document).ready(function() {
     $("#staff-acc-prop").on('click', function () {
         staff.handleModal("approved");
     });
+
+    $("#staff-defense-prop").on('click', function () {
+        staff.defenseDateModal();
+    })
 
 });
