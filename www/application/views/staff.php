efb1aef5db5f3a136bdabd15d67e19b30b2ae819
todd <todd.bednarczyk@gmail.com>
2018-04-30 00:35:08 -0400

    Changed name of "defense dates" button to "View All Capstones"

--- www/application/views/staff.php
+++ www/application/views/staff.php
@@ -26,7 +26,7 @@
                 <button class="staff-btn-options" id="staff-rej-prop" type="button" name="staff-rej-prop">Rejected Proposals</button>
                 <button class="staff-btn-options" id="staff-acc-prop" type="button" name="staff-acc-prop">Accepted Proposals</button>
                 <button class="staff-btn-options" id="staff-complete-proj" type="button" name="staff-complete-proj">Completed Projects</button>
-                <button class="staff-btn-options" id="staff-defense-prop" type="button" name="staff-defense-dates">Defense Dates</button>
+                <button class="staff-btn-options" id="staff-defense-prop" type="button" name="staff-defense-dates">View All Capstones</button>
             </div>
             <div class="col-sm-2"></div>
         </div>
